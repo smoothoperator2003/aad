7daac2165c3e977844b552bd94f862f3306c7e8b
Smooth Operator <143164739+smoothoperator2003@users.noreply.github.com>
2024-04-01 15:20:58 +0530

    Add files via upload

---
+++ practical 9 create a simple html hello world project using angularjs framework and applying controller ng model and expressions.ps1
@@ -0,0 +1,22 @@
+# Practical 9 create a simple html hello world project using angularjs framework and applyng controller ng model and expressions
+
+
+# check whether node.js is downloaded or not
+npm --version
+
+# install angular cli globally
+npm install -g @angular/cli
+
+# create angular app
+ng new my-app
+
+# to run scripts
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
+
+cd my-app
+
+# Angular CLI compiles the application, starts a development server, and watches for changes to your files.
+ng serve
+
+# run the app 
+angular app -> http://localhost:4200/
