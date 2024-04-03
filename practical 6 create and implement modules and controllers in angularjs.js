19b8cb3183c6f20409a02995916f2c86f019b27d
Smooth Operator <143164739+smoothoperator2003@users.noreply.github.com>
2024-04-03 23:57:36 +0530

    Update practical 6 create and implement modules and controllers in angularjs.js

--- practical 6 create and implement modules and controllers in angularjs.js
+++ practical 6 create and implement modules and controllers in angularjs.js
@@ -1,14 +1,17 @@
 // practical 6 write a program to create and implement modules and controllers in angular js
 
 
-var app = angular.module('DemoApp',[]);
+var app = angular.module('DemoApp', []);
+
 app.controller('DemoController', function ($scope) { 
- $scope.list = ['A', 'E', 'I', 'O', 'U']; 
- $scope.choice = 'Your choice is: GeeksforGeeks'; 
- $scope.ch = function (choice) { 
- $scope.choice = "Your choice is: " + choice; 
- }; 
- $scope.c = function () { 
- $scope.choice = "Your choice is: " + $scope.mychoice; 
- }; 
+    $scope.list = ['A', 'E', 'I', 'O', 'U']; 
+    $scope.choice = 'Your choice is: GeeksforGeeks'; 
+    
+    $scope.ch = function (choice) { 
+        $scope.choice = "Your choice is: " + choice; 
+    }; 
+    
+    $scope.c = function () { 
+        $scope.choice = "Your choice is: " + $scope.mychoice; 
+    }; 
 });
